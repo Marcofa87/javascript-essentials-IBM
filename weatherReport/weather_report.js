d8ae5df5099b2e28a213c4f331c4c260b7ae0b3a
Marcofa87 <marco.falasca87@gmail.com>
2024-12-18 10:12:44 -0500

    message

---
+++ weatherReport/weather_report.js
@@ -0,0 +1,23 @@
+function showweatherDetails(event) {
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const apiKey = 'ae719adad25c200765e3f9c37fd08f73'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    console.log(apiUrl);
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                                <p>Temperature: ${data.main.temp} &#8451;</p>
+                                <p>Weather: ${data.weather[0].description}</p>`;
+        }).catch(error => {
+            console.error('Error fetching weather:', error);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+        });
+
+
+}
+document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
